a448634dce780479cccc665a135e8b5be8bb3837
Thomas Hamelin <thomashamelin27@outlook.de>
2022-11-20 21:31:39 +0100

    Intial Commit

---
+++ src/main/java/com/univ/kandan/KandanApplication.java
@@ -0,0 +1,13 @@
+package com.univ.kandan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KandanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KandanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univ/kandan/KandanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ.kandan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KandanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
