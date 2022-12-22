be4f5f17c0d9336a1148fac08c85d45dd4598c15
Coucoba <lucas.ficker@gmail.com>
2022-12-22 17:16:28 +0100

    drag and drop complete (remove useless code)

--- src/main/java/com/univ/kanban/controllers/TaskController.java
+++ src/main/java/com/univ/kanban/controllers/TaskController.java
@@ -1,16 +1,12 @@
 package com.univ.kanban.controllers;
 
-import com.univ.kanban.models.Kanban;
 import com.univ.kanban.models.KanbanColumn;
 import com.univ.kanban.models.Task;
 import com.univ.kanban.services.ColumnService;
-import com.univ.kanban.services.KanbanService;
 import com.univ.kanban.services.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,8 +24,6 @@ public class TaskController {
         KanbanColumn column = columnService.findById(id_column);
         task.setColumn(column);
         taskService.save(task);
-        Kanban kanban = column.getKanban();
-        Long kanban_id = kanban.getId();
     }
 
 }
